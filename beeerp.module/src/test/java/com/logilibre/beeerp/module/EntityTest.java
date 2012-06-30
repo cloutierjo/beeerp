8a5c252839d20b63458fcc5c1fc2671432b07801
cloutier jo <cloutier.jo@logilibre.com>
2012-06-30 00:11:04 -0400

    add failling unit test (for jenkins test)

---
+++ beeerp.module/src/test/java/com/logilibre/beeerp/module/EntityTest.java
@@ -0,0 +1,54 @@
+package com.logilibre.beeerp.module;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class EntityTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public final void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testEntity() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testEqualsObject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testGetFields() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testToString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testGetData() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public final void testSetData() {
+		fail("Not yet implemented");
+	}
+
+}
