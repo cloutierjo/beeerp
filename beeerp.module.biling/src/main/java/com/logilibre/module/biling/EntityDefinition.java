37595eaaa1ee3111225bd9bd4c8709ac28ce1389
Jonatan Cloutier <cloutier.jo@gmail.com>
2016-10-24 23:59:10 -0400

    Fix static inner class

--- beeerp.module.biling/src/main/java/com/logilibre/module/biling/EntityDefinition.java
+++ beeerp.module.biling/src/main/java/com/logilibre/module/biling/EntityDefinition.java
@@ -4,24 +4,24 @@ import java.util.List;
 
 public class EntityDefinition {
 
-	protected class Client{
+	static protected class Client {
 		String name;
 		String email;
-		
-		
+
+
 	}
-	
-	protected class Bill{
+
+	static protected class Bill {
 		Client client;
 		List<BillItem> billItems;
 		//@Calculated(TotalCalcule.class)
 		double total;
 		boolean sent;
 		boolean paid;
-		
+
 	}
 
-	protected class BillItem {
+	static protected class BillItem {
 		String name;
 		//@Min(0)
 		double price;
